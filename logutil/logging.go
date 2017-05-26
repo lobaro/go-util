b854aaeb0fabc32b55ee60ca921b31a021be7945
Tobias Kaupat <code@niondir.de>
2017-05-26 12:27:52 +0200

    Logging util

---
+++ logutil/logging.go
@@ -0,0 +1,38 @@
+package logutil
+
+import (
+	"io"
+	"sync"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/fatih/color"
+)
+
+var Output = &ThreadsafeWriter{
+	Writer: color.Output,
+	Mutex:  &sync.Mutex{},
+}
+
+type ThreadsafeWriter struct {
+	Writer io.Writer
+	Mutex  *sync.Mutex
+}
+
+func (w ThreadsafeWriter) Write(p []byte) (n int, err error) {
+	w.Mutex.Lock()
+	n, err = w.Writer.Write(p)
+	w.Mutex.Unlock()
+	return
+}
+
+// Setup default logging.
+// Logrus with color output
+func SetupLogging() {
+	logrus.SetOutput(Output)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:   true,
+		FullTimestamp: false,
+	})
+
+	logrus.Info("Did setup default logging")
+}
