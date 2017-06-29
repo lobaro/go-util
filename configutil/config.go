b20e36c4e266227a69c35dfad1d7c148f4c72371
Tobias Kaupat <code@niondir.de>
2017-06-29 10:41:11 +0200

    interpret config parameter more sane

--- configutil/config.go
+++ configutil/config.go
@@ -1,9 +1,9 @@
 package configutil
 
 import (
-	"errors"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -12,19 +12,30 @@ import (
 
 func Setup(cmd *cobra.Command, requireFile bool) *viper.Viper {
 	cfg := viper.GetViper()
-	configName, err := cmd.Flags().GetString("config")
+	configParam, err := cmd.Flags().GetString("config")
+	fileName := configParam
+	configType := ""
+
+	dir := filepath.Dir(configParam)
+	fileName = filepath.Base(configParam)
+
+	configType = filepath.Ext(configParam)
+	configType = strings.TrimLeft(configType, ".")
+	fileName = strings.TrimSuffix(fileName, configType) // Must be without extenstion
+
+	if configType == "" {
+		configType = "json"
+	}
+
 	if err != nil {
 		logrus.WithError(err).Debug("Failed to read config flag")
 	} else {
-		cfg.SetConfigName(configName)
+		cfg.SetConfigName(fileName)
 	}
-	cfg.SetConfigType("json")
-	// Add working dir
-	dir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
+	cfg.SetConfigType(configType)
+
 	cfg.AddConfigPath(dir)
+	logrus.WithField("dir", dir).WithField("file", fileName+"."+configType).WithField("type", configType).Info("Loading config")
 	// Add binary dir
 	dir, err = filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
@@ -34,8 +45,12 @@ func Setup(cmd *cobra.Command, requireFile bool) *viper.Viper {
 	cfg.BindPFlags(cmd.Flags())
 
 	err = cfg.ReadInConfig()
-	if err != nil && requireFile {
-		logrus.Fatal(errors.New(err.Error() + ": Failed to read config file"))
+	if err != nil {
+		if requireFile {
+			logrus.WithError(err).Fatal("Failed to read config file")
+		} else {
+			logrus.WithError(err).Info("Failed to read config file")
+		}
 	}
 	return cfg
 }
