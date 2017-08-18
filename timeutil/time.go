eff5081c8522796b62e82df9def5c47d946c7b0c
Tobias Kaupat <code@niondir.de>
2017-08-18 09:35:12 +0200

    Timeutil

--- timeutil/time.go
+++ timeutil/time.go
@@ -2,20 +2,17 @@ package timeutil
 
 import (
 	"time"
-
-	"github.com/Sirupsen/logrus"
 )
 
 const (
 	BERLIN = "Europe/Berlin"
 )
 
-func SetLocalTimezone(location string) {
+func SetLocalTimezone(location string) error {
 	berlinLocation, err := time.LoadLocation("Europe/Berlin")
 	if err != nil {
-		logrus.WithError(err).Warn("Failed to load timezone for Europe/Berlin using system default.")
-	} else {
-		time.Local = berlinLocation
-		logrus.Info("Loaded local timezone: Europe/Berlin")
+		return err
 	}
+	time.Local = berlinLocation
+	return nil
 }
