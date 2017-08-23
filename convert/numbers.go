c440d75845d28ece8f2a9adacd4f0dba1d63a104
Tobias Kaupat <code@niondir.de>
2017-08-23 17:31:21 +0200

    Convert to float method

---
+++ convert/numbers.go
@@ -0,0 +1,47 @@
+package convert
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strconv"
+)
+
+var floatType = reflect.TypeOf(float64(0))
+var stringType = reflect.TypeOf("")
+
+func ToFloat(val interface{}) (float64, error) {
+	switch i := val.(type) {
+	case float64:
+		return i, nil
+	case float32:
+		return float64(i), nil
+	case int64:
+		return float64(i), nil
+	case int32:
+		return float64(i), nil
+	case int:
+		return float64(i), nil
+	case uint64:
+		return float64(i), nil
+	case uint32:
+		return float64(i), nil
+	case uint:
+		return float64(i), nil
+	case string:
+		return strconv.ParseFloat(i, 64)
+	default:
+		v := reflect.ValueOf(val)
+		v = reflect.Indirect(v)
+		if v.Type().ConvertibleTo(floatType) {
+			fv := v.Convert(floatType)
+			return fv.Float(), nil
+		} else if v.Type().ConvertibleTo(stringType) {
+			sv := v.Convert(stringType)
+			s := sv.String()
+			return strconv.ParseFloat(s, 64)
+		} else {
+			return math.NaN(), fmt.Errorf("Can't convert %v to float64", v.Type())
+		}
+	}
+}
