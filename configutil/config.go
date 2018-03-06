36328606fe8f9c2be6ad9d96c6acb941635f4bed
Tobias Kaupat <tobiall@niondir.de>
2018-03-06 18:31:42 +0100

    Fix config file loading

--- configutil/config.go
+++ configutil/config.go
@@ -12,7 +12,10 @@ import (
 
 func Setup(cmd *cobra.Command, requireFile bool) *viper.Viper {
 	cfg := viper.GetViper()
-	configParam, err := cmd.Flags().GetString("config")
+	configParam := "config.yaml"
+	if cmd != nil {
+		configParam, _ = cmd.Flags().GetString("config")
+	}
 	fileName := configParam
 	configType := ""
 
@@ -21,23 +24,19 @@ func Setup(cmd *cobra.Command, requireFile bool) *viper.Viper {
 
 	configType = filepath.Ext(configParam)
 	configType = strings.TrimLeft(configType, ".")
-	fileName = strings.TrimSuffix(fileName, configType) // Must be without extenstion
+	fileName = strings.TrimSuffix(fileName, filepath.Ext(fileName)) // Must be without extension
 
 	if configType == "" {
 		configType = "json"
 	}
 
-	if err != nil {
-		logrus.WithError(err).Debug("Failed to read config flag")
-	} else {
-		cfg.SetConfigName(fileName)
-	}
+	cfg.SetConfigName(fileName)
 	cfg.SetConfigType(configType)
 
 	cfg.AddConfigPath(dir)
 	logrus.WithField("dir", dir).WithField("file", fileName+"."+configType).WithField("type", configType).Info("Loading config")
 	// Add binary dir
-	dir, err = filepath.Abs(filepath.Dir(os.Args[0]))
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		panic(err)
 	}
@@ -57,7 +56,9 @@ func Setup(cmd *cobra.Command, requireFile bool) *viper.Viper {
 
 	// Read in config
 
-	cfg.BindPFlags(cmd.Flags())
+	if cmd != nil {
+		cfg.BindPFlags(cmd.Flags())
+	}
 
 	err = cfg.ReadInConfig()
 	if err != nil {
