5497c5b3c49a5c943a12a403a2a9884c51c9a1a2
Tobias Kaupat <tobiall@niondir.de>
2018-08-06 11:57:40 +0200

    optional config file

--- configutil/config.go
+++ configutil/config.go
@@ -10,57 +10,64 @@ import (
 	"github.com/spf13/viper"
 )
 
-func Setup(cmd *cobra.Command) (*viper.Viper, error) {
+// configParam default is "config.yml", empty string will not load a file
+func Setup(cmd *cobra.Command, configParam string) (*viper.Viper, error) {
 	cfg := viper.GetViper()
-	configParam := "config.yml"
-	if cmd != nil {
-		configParam, _ = cmd.Flags().GetString("config")
-	}
-	fileName := configParam
-	configType := ""
 
-	dir := filepath.Dir(configParam)
-	fileName = filepath.Base(configParam)
-
-	configType = filepath.Ext(configParam)
-	configType = strings.TrimLeft(configType, ".")
-	fileName = strings.TrimSuffix(fileName, filepath.Ext(fileName)) // Must be without extension
-
-	if configType == "" {
-		configType = "yml"
-	}
-
-	cfg.SetConfigName(fileName)
-	cfg.SetConfigType(configType)
-
-	cfg.AddConfigPath(dir)
-	logrus.WithField("dir", dir).WithField("file", fileName+"."+configType).WithField("type", configType).Info("Loading config")
-	// Add binary dir
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		panic(err)
-	}
-	cfg.AddConfigPath(dir)
-
-	dir, err = filepath.Abs(filepath.Join(filepath.Dir(os.Args[0]), "/config"))
-	if err != nil {
-		panic(err)
-	}
-	cfg.AddConfigPath(dir)
-
-	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(
+	cfg.AutomaticEnv()
+	cfg.SetEnvKeyReplacer(strings.NewReplacer(
 		"-", "_",
-		".", "__",
+		".", "__", // TODO: switch to . -> _
 	))
-
-	// Read in config
 
 	if cmd != nil {
 		cfg.BindPFlags(cmd.Flags())
 	}
 
-	err = cfg.ReadInConfig()
+	if cmd != nil && configParam == "" {
+		configParam, _ = cmd.Flags().GetString("config")
+	}
 
-	return cfg, err
+	hasFileConfig := configParam != ""
+
+	if hasFileConfig {
+		fileName := configParam
+		configType := ""
+
+		dir := filepath.Dir(configParam)
+		fileName = filepath.Base(configParam)
+
+		configType = filepath.Ext(configParam)
+		configType = strings.TrimLeft(configType, ".")
+		fileName = strings.TrimSuffix(fileName, filepath.Ext(fileName)) // Must be without extension
+
+		if configType == "" {
+			configType = "yml"
+		}
+
+		cfg.SetConfigName(fileName)
+		cfg.SetConfigType(configType)
+
+		cfg.AddConfigPath(dir)
+		logrus.WithField("dir", dir).WithField("file", fileName+"."+configType).WithField("type", configType).Info("Loading config")
+		// Add binary dir
+		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			panic(err)
+		}
+		cfg.AddConfigPath(dir)
+
+		dir, err = filepath.Abs(filepath.Join(filepath.Dir(os.Args[0]), "/config"))
+		if err != nil {
+			panic(err)
+		}
+		cfg.AddConfigPath(dir)
+
+		err = cfg.ReadInConfig()
+		if err != nil {
+			return cfg, nil
+		}
+	}
+
+	return cfg, nil
 }
